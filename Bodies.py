078edbe1364eb28fdb0b928fb13dd0aaedadf3de
Andrew Cleland <alcleland@gmail.com>
2014-08-20 17:45:48 -0700

    Expanded Bodies.py

--- Bodies.py
+++ Bodies.py
@@ -11,19 +11,58 @@ class Thing():
 
 class Body(Thing):
     # A physical body that allows for translational motion
-    def __init__(self, x, y, vx=0, vy=0, mass=1):
+    def __init__(self, x, y, vx=0, vy=0, ax=0, ay=0, mass=1):
         super(Body, self).__init__(x, y)
         self.mass = mass
         self.vel = vector(vx, vy)
-        self.acc = vector(0, 0)  # Things start off with no acceleration
-
-    def update(self, force=vector(0,0)):
-        self.acc = force / self.mass   # Newton's 2nd law
-         
+        self.acc = vector(ax, ay)  # Things start off with no acceleration
     
+    def setBounds(self):
+        pass
+
+    def update(self, dt=1, force=vector(0, 0)):
+        #self.acc = force / self.mass   # Newton's 2nd law
+        self.vel += self.acc * dt
+        self.pos += self.vel * dt
+        self.setBounds()
+
+    def velflipx(self):
+        self.vel = vector(-self.vel.x, self.vel.y)
+    
+    def velflipy(self):
+        self.vel = vector(self.vel.x, -self.vel.y)
+         
 class Ball(Body):
     # A body whose shape is a circle.
-    def __init__(self, x, y, vx=0, vy=0, ax=0, ay=0, mass=1, radius)
+    def __init__(self, r, x, y, vx=0, vy=0, ax=0, ay=0, mass=1):
+        super(Ball, self).__init__(x, y, vx, vy, ax, ay, mass)
+        self.radius = r
+        self.setBounds()
+    
+    def setBounds(self):
+        # Marks the boundary edges of the ball. 
+        self.left = self.pos.x - self.radius
+        self.right = self.pos.x + self.radius
+        self.top = self.pos.y - self.radius
+        self.bottom = self.pos.y + self.radius
+    
+    def overlaps(self, other):
+        # Detects if it overlaps with another ball, and if so, corrects it.
+        
+        vecdiff = other.pos - self.pos  # Vector pointing from self to other
+        dist = vecdiff.mag()      # Distance between centers
+        overlapping = dist < self.radius + other.radius
+        
+        if overlapping:
+            # Adjust ball positions so that they are just touching on the edges
+            overlap = self.radius + other.radius - dist
+            # Move self by half the overlap distance:
+            self.pos -= vecdiff / dist * overlap / 2
+            # Move other by half the overlap distance:
+            other.pos += vecdiff / dist * overlap/2
+        
+        return overlapping
+
 
 
 if __name__ == '__main__':
@@ -32,3 +71,10 @@ if __name__ == '__main__':
 
     thing = Thing(3,4)
     thing.pos.print()
+
+    ball = Ball(5, 5, 6)
+    ball.pos.print()
+    ball.vel.print()
+    print(ball.mass)
+    print(ball.radius)
+
