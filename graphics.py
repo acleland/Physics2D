eb224d73c316b8a1d79f6402c53660a4ed620076
Andrew Cleland <alcleland@gmail.com>
2014-08-20 18:22:36 -0700

    Comments added to graphics.py

--- graphics.py
+++ graphics.py
@@ -1,4 +1,6 @@
-# Graphics prototype 
+# Copyright (c) 2014 Andrew Cleland
+# Graphics prototype for Physics 2D
+
 import sys, pygame
 from Bodies import *
 from vector import *
@@ -6,12 +8,14 @@ from random import random
 
 pygame.init()
 
+# Constants:
 size = width, height = 640, 480
 speed = vx, vy = 2, 2
 black = 0, 0, 0
 blue = 0, 0, 255
 radius = 10
 
+# User input:
 n = int(input('Number of bodies: '))
 gravity = float(input('Set gravity: '))
 collisions_on = 'y' == input('Collision detection? (y/n?): ')
@@ -23,13 +27,21 @@ for _ in range(n):
     bodies.append(Ball(r=radius, x=random()*width, y=random()*height,
                     vx=random()*vx, vy=random()*vy, ay=gravity))
 
+# Draw bodies and animate them:
 while True:
+    # GUI exit
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     
+    # Start by filling in the background color
     screen.fill(black)
+
+    # Draw the bodies
     for body in bodies:
+        # Round positions to integer values
         pos = round(body.pos.x), round(body.pos.y)
+        
+        # Move body according to current velocity, acceleration
         body.update(dt=.1)
         
         if collisions_on:
@@ -38,16 +50,16 @@ while True:
                 # Don't compare a body to itself
                 if other == body:
                     continue
+                # Fix overlaps
                 body.overlaps(other)
 
-            
-        
         # Bounce off edges
         if body.left < 0 or body.right > width:
             body.velflipx()
         if body.top < 0 or body.bottom > height:
             body.velflipy()
         
+        # Draw the body
         pygame.draw.circle(screen, blue, pos, round(body.radius))
 
     #screen.blit(ball, ballrect)
